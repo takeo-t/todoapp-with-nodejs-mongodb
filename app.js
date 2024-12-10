61708c36ec462120dbedf35a0ef2b173e78c7307
takeo-t <takeo-t@118satellite.com>
2024-12-11 00:26:24 +0900

    31

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 const taskRoutye = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 const PORT = 5000;
 
 // ルーティングの設定
 app.use("/api/v1/tasks", taskRoutye);
 
-app.listen(PORT, console.log("サーバーが起動しました"));
+// データベースとの接続
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, console.log("サーバーが起動しました"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
