d68af7a11a0cda38f7afffd0abfc3b598878628f
takeo-t <takeo-t@118satellite.com>
2024-12-10 22:54:34 +0900

    26

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
 const express = require("express");
 const app = express();
+const taskRoutye = require("./routes/tasks");
+
 const PORT = 5000;
 
-app.get("/api/v1/tasks", (req, res) => {
-  res.send("タスクをすべて取得しました");
-});
-
-app.post("/api/v1/tasks", (req, res) => {
-  res.send("タスクを新規作成しました");
-});
-
-app.get("/api/v1/tasks/:id", (req, res) => {
-  res.send("ある特定のタスクを取得しました");
-});
-
-app.patch("/api/v1/tasks/:id", (req, res) => {
-  res.send("ある特定のタスクを更新しました");
-});
-
-app.delete("/api/v1/tasks/:id", (req, res) => {
-  res.send("ある特定のタスクを削除しました");
-});
+// ルーティングの設定
+app.use("/api/v1/tasks", taskRoutye);
 
 app.listen(PORT, console.log("サーバーが起動しました"));
