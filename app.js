584c5bb6a2823e0c5897c4e01235f0047ad80e84
takeo-t <takeo-t@118satellite.com>
2024-12-10 22:46:09 +0900

    26

--- app.js
+++ app.js
@@ -2,4 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+app.get("/api/v1/tasks", (req, res) => {
+  res.send("タスクをすべて取得しました");
+});
+
+app.post("/api/v1/tasks", (req, res) => {
+  res.send("タスクを新規作成しました");
+});
+
+app.get("/api/v1/tasks/:id", (req, res) => {
+  res.send("ある特定のタスクを取得しました");
+});
+
+app.patch("/api/v1/tasks/:id", (req, res) => {
+  res.send("ある特定のタスクを更新しました");
+});
+
+app.delete("/api/v1/tasks/:id", (req, res) => {
+  res.send("ある特定のタスクを削除しました");
+});
+
 app.listen(PORT, console.log("サーバーが起動しました"));
