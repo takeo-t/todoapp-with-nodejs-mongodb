ec1f777fb41b33b5bec57fbd6fee3555eba696b3
takeo-t <takeo-t@118satellite.com>
2024-12-11 23:45:10 +0900

    33

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,16 @@
-const { get } = require("../routes/tasks");
+const Task = require("../models/Task");
 
 const getAllTasks = (req, res) => {
   res.send("タスクをすべて取得しました");
 };
 
-const createTask = (req, res) => {
-  res.send("タスクを新規作成しました");
+const createTask = async (req, res) => {
+  try {
+    const createTask = await Task.create(req.body);
+    res.status(201).json({ createTask });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const getSingleTask = (req, res) => {
