1eed24a1bbbaca4fc8faf4d3bee94345beaae5e1
takeo-t <takeo-t@118satellite.com>
2024-12-12 22:32:51 +0900

    38

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -18,16 +18,52 @@ const createTask = async (req, res) => {
   }
 };
 
-const getSingleTask = (req, res) => {
-  res.send("ある特定のタスクを取得しました");
+const getSingleTask = async (req, res) => {
+  try {
+    const getSingleTask = await Task.findOne({ _id: req.params.id });
+
+    if (!getSingleTask) {
+      return res.status(404).json({ id: `${req.params.id}は存在しません` });
+    }
+
+    res.status(201).json({ getSingleTask });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const updateTask = (req, res) => {
-  res.send("ある特定のタスクを更新しました");
+const updateTask = async (req, res) => {
+  try {
+    const updateTask = await Task.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    if (!updateTask) {
+      return res.status(404).json({ id: `${req.params.id}は存在しません` });
+    }
+
+    res.status(201).json({ updateTask });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("ある特定のタスクを削除しました");
+const deleteTask = async (req, res) => {
+  try {
+    const deleteTask = await Task.findOneAndDelete({ _id: req.params.id });
+
+    if (!deleteTask) {
+      return res.status(404).json({ id: `${req.params.id}は存在しません` });
+    }
+
+    res.status(201).json({ deleteTask });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
